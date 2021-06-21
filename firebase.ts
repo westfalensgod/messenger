d59b45061e3b73b017fea4d12384f6a9141b700a
westfalensgod <westfalensgod@gmail.com>
2021-06-21 21:11:58 +0300

    fix: rm api keys from public xD

--- firebase.ts
+++ firebase.ts
@@ -2,14 +2,15 @@ import firebase from "firebase";
 import "firebase/firestore";
 import "firebase/auth";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAau3YKLlmm7eS89YVlRpRPaWtKNPpo58U",
-  authDomain: "messenger-bfaf1.firebaseapp.com",
-  projectId: "messenger-bfaf1",
-  storageBucket: "messenger-bfaf1.appspot.com",
-  messagingSenderId: "258497643739",
-  appId: "1:258497643739:web:1017b8397d5879b1d5b8fe"
-};
+    apiKey: process.env.API_KEY,
+    authDomain: "messenger-2-fd1c5.firebaseapp.com",
+    projectId: process.env.PROJECT_ID,
+    storageBucket: "messenger-2-fd1c5.appspot.com",
+    messagingSenderId: process.env.SENDER_ID,
+    appId: process.env.APP_ID
+  };
 
 const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
 const db = app.firestore();
